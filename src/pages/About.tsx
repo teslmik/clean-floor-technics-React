72d081db4dc84beef7327d45886f38d6fa25c5d2
Teslmik <teslmik@gmail.com>
2023-01-06 19:54:31 +0200

    Update title on pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import { useLocation } from 'react-router-dom';
 
 import Breadcrumbs from '../components/Breadcrumbs';
@@ -15,6 +16,28 @@ const About: React.FC = () => {
       <div className="about__content">
         <Breadcrumbs titleBlock={'Про нас'} />
         <h2 className="about__title">Про нас</h2>
+        <img src="/assets/img/MadeInBritainWebPagePhoto.png" alt="Made In Britain" />
+        <div className="content-about__text">
+          <ReactMarkdown>
+            **Truvox International** — провідний постачальник комерційних і промислових машин
+            для обслуговування підлог. Компанія заснована в Саутгемптоні (Southampton),
+            Великобританія. З дня виробництва нашого першого електричного полірувальника на
+            початку 1960-х років Truvox успішно розширила як асортимент, так і території
+            поширення продукції, в даний час, кількість країн досягає більш ніж 70 по всьому
+            світу.
+          </ReactMarkdown>
+          <ReactMarkdown>
+            Truvox пропонує широкий асортимент брендів, що добре зарекомендували себе, включаючи
+            полірувальники **Orbis**, килимові екстрактори **Hydromist**, пилососи **Valet**,
+            підлогонатирачі **Multiwash** і роторні машини **Cimex** з трьома дисковими щітками.
+          </ReactMarkdown>
+          <ReactMarkdown>
+            Офіційним дистриб'ютером **Truvox International** в Україні є інтернет-магазин
+            **Clean Floor Techics**. Ми на ринку клінінгу та хімчистки більше 10 років та маємо
+            дуже великий досвід в чищені підлог усіх видів, а також знаємось на різноманітних
+            видах прибиральної техніки.
+          </ReactMarkdown>
+        </div>
       </div>
     </section>
   );
