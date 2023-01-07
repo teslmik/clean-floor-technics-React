7dbcf5c2c9b0932865bb61257c50f80623e51547
Teslmik <teslmik@gmail.com>
2023-01-07 19:50:04 +0200

    Optimisation

--- src/components/CircleLoader.tsx
+++ src/components/CircleLoader.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const CircleLoader: React.FC = () => {
   return (
-    <section className="__container">
-      <div className="loader">Loading...</div>
-    </section>
+    <div className="loader">Loading...</div>
   )
 }
 
