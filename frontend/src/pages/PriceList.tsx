dc86895cbffbdd0022574b882ca4ee5f65af9482
teslmik <teslmik@gmail.com>
2024-09-21 12:35:51 +0300

    fix download butoon position

--- frontend/src/pages/PriceList.tsx
+++ frontend/src/pages/PriceList.tsx
@@ -1,25 +1,43 @@
 import React from "react";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { Button } from "@mui/material";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 
 import priceList from "/assets/docs/PriceListTruvox_2024.pdf";
 
 export const PriceList: React.FC = () => {
-  const isIOS = () => {
+  const isMobileOrSafari = () => {
+    const userAgent = navigator.userAgent;
     return (
-      /iPad|iPhone|iPod/.test(navigator.userAgent) ||
-      (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
+      /iPhone|iPad|iPod|Android/i.test(userAgent) ||
+      (userAgent.includes("Safari") && !userAgent.includes("Chrome"))
     );
   };
 
   return (
     <div className="priceList">
-      {isIOS() ? (
-        <Worker
-          workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
-        >
-          <Viewer fileUrl={priceList} />
-        </Worker>
+      {isMobileOrSafari() ? (
+        <>
+          <Button
+            href={priceList}
+            download="PriceListTruvox_2024.pdf"
+            variant="contained"
+            sx={{
+              position: "sticky",
+              top: 0,
+              left: "100%",
+              transform: "translate(-30px, 60px)",
+              zIndex: 100,
+            }}
+          >
+            Скачать прайс-лист
+          </Button>
+          <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
+          >
+            <Viewer enableSmoothScroll fileUrl={priceList} />
+          </Worker>
+        </>
       ) : (
         <iframe src={priceList} width="100%" height="750px" title="priseList" />
       )}
